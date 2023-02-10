dae2a48f72fb0a7227268485c8a1c17907f9ea47
Soraia Barbosa <so_raia_super@hotmail.com>
2023-02-10 07:42:58 -0300

    evento de soma

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
-const robotron = document.querySelector("#robotron").addEventListener("click", () => {
-    console.log("Fui clicado")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
+
+
+somar.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) + 1
 })
 
-function dizOi(nome) {
-    console.log("Oi" +  nome)
-}
-
-dizOi("Pedro")
+subtrair.addEventListener("click", (evento) => {
+    braco.value = parseInt(braco.value) - 1
+})
