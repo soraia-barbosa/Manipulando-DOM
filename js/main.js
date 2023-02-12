627436124ceb769aa90084925fa2b15a965386e4
Soraia Barbosa <so_raia_super@hotmail.com>
2023-02-11 21:31:00 -0300

    condicionais

--- js/main.js
+++ js/main.js
@@ -2,11 +2,19 @@ const subtrair = document.querySelector("#subtrair")
 const somar = document.querySelector("#somar")
 const braco = document.querySelector("#braco")
 
+const controle = document.querySelectorAll(".controle-ajuste")
 
-somar.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) + 1
-})
 
-subtrair.addEventListener("click", (evento) => {
-    braco.value = parseInt(braco.value) - 1
-})
+somar.addEventListener("click", () => {manipulaDados ("somar")})
+    
+
+subtrair.addEventListener("click", () => {manipulaDados ("subtrair")})
+    
+
+function manipulaDados(operação){
+    if(operação === "subtrair"){
+        braco.value = parseInt(braco.value) - 1
+    }else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
